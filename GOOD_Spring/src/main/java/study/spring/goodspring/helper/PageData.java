7a37f8e99356ec3c1342ac177f4018a996af5a8b
Your Name <sanghwa318@gmail.com>
2021-07-02 10:45:44 +0900

    helper클래스 추가, root-context,pom.xml에 설정값 추가

--- GOOD_Spring/src/main/java/study/spring/goodspring/helper/PageData.java
+++ GOOD_Spring/src/main/java/study/spring/goodspring/helper/PageData.java
@@ -1,31 +1,32 @@
 package study.spring.goodspring.helper;
 
+
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Data
 @Slf4j
 public class PageData {
-	/***** GET파라미터로 처리할 값 *****/
+	/***** GET 파라미터로 처리할 값 *****/
 	private int nowPage = 1; // 현재 페이지 번호
 
 	/***** DB에서 조회한 결과 값 *****/
 	private int totalCount = 0; // 전체 글 개수 조회
 
 	/***** 개발자가 정의해야 하는 값 *****/
-	private int listCount = 10; // 한 페이지에 보여질 글의 목록 수
+	private int listCount = 10;// 한 페이지에 보여질 글의 목록 수
 	private int groupCount = 5; // 한 그룹에 표시할 페이지번호 개수
 
 	/***** 연산처리가 필요한 값 *****/
-	private int totalPage = 0; // 전체 페이지 수
+	private int totalPage = 0;// 전체 페이지 수
 	private int startPage = 0; // 현재 그룹의 시작 페이지
 	private int endPage = 0; // 현재 그룹의 마지막 페이지
 	private int prevPage = 0; // 이전 그룹의 마지막 페이지
-	private int nextPage = 0; // 이전 그룹의 첫 페이지
-	private int offset = 0; // MySQL의 Limit 시작 위치
+	private int nextPage = 0; // 이전 그룹의 첫 번째 페이지
+	private int offset = 0; // MySQL의 LIMIT 시작 위치
 
 	/**
-	 * 객체가 만들어짐과 동시에 필요한 연산을 일괄 수행하는 생성자.
+	 * 객체가 만들어짐과 동시에 필요한 연산을 일괄 수행하는 생성자
 	 */
 	public PageData(int nowPage, int totalCount, int listCount, int groupCount) {
 		// 멤버변수에 파라미터 복사
@@ -41,11 +42,9 @@ public class PageData {
 		if (nowPage < 0) {
 			nowPage = 1;
 		}
-
 		if (nowPage > totalPage) {
 			nowPage = totalPage;
 		}
-
 		// 현재 페이징 그룹의 시작 페이지 번호
 		startPage = ((nowPage - 1) / groupCount) * groupCount + 1;
 
@@ -56,25 +55,21 @@ public class PageData {
 		if (endPage > totalPage) {
 			endPage = totalPage;
 		}
-
 		// 이전 그룹의 마지막 페이지
 		if (startPage > groupCount) {
 			prevPage = startPage - 1;
 		} else {
 			prevPage = 0;
 		}
-
-		// 다음 그룹의 첫 페이지
-		if (endPage < totalPage) {
-			nextPage = endPage + 1;
-		} else {
-			nextPage = 0;
+		//다음 그룹의 첫 페이지
+		if (endPage<totalPage) {
+			nextPage=endPage+1;
+		}else {
+			nextPage=0;
 		}
-
-		// 검색 범위의 시작 위치
-		offset = (nowPage - 1) * listCount;
-
-		// 계산 결과를 로그로 기록한다.
+		//검색 범위의 시작 위치
+		offset= (nowPage-1)*listCount;
+		//계산 결과를 로그로 기록한다
 		log.debug(this.toString());
 	}
 }
