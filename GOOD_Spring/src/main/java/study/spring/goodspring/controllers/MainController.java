24d5f1dbe6945e1da689b2f6d0452165ae9c9666
chaewon <chaewonjeong0703@gmail.com>
2021-07-30 17:11:04 +0900

    [정채원] search.jsp > 통합 검색 페이지 Mapper

--- GOOD_Spring/src/main/java/study/spring/goodspring/controllers/MainController.java
+++ GOOD_Spring/src/main/java/study/spring/goodspring/controllers/MainController.java
@@ -81,5 +81,16 @@ public class MainController {
 		return "myPage/myPage_accountEdit";
 	}
 	
+	/**
+	 * 마이 페이지 회원 수정 페이지
+	 * 
+	 * @return String
+	 */
+	@RequestMapping(value = "/myPage/myPage_accountEdit.do", method = RequestMethod.GET)
+	public String search() {
+		
+		return "myPage/myPage_accountEdit";
+	}
+	
 
 }
