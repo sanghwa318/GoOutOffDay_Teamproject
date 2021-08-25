2c51c0091fc5184ffd5b98b8792125e75f78684a
Your Name <sanghwa318@gmail.com>
2021-08-25 18:45:10 +0900

    [이상화]나만의코스 글 삭제시 댓글 삭제.

--- GOOD_Spring/src/main/java/study/spring/goodspring/controllers/MyCourseController.java
+++ GOOD_Spring/src/main/java/study/spring/goodspring/controllers/MyCourseController.java
@@ -63,7 +63,7 @@ public class MyCourseController {
 
 		// 1) 페이지 구현에 필요한 변수값 생성
 		int totalCount = 0; // 전체 게시글 수
-		int listCount = 6; // 한 페이지당 표시할 항목 수
+		int listCount = 12; // 한 페이지당 표시할 항목 수
 		int pageCount = 5; // 한 그룹당 표시할 페이지 번호 수
 
 		// 2) 데이터 조회하기
